b2666eccf39ec7fbcefc5764eee7d146d03c7bfc
nourouhichi <1417@holbertonschool.com>
2020-03-17 17:23:20 +0100

    hexadecimal printing functions

---
+++ _hexa.c
@@ -0,0 +1,70 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *is_octal - prints the octal
+ *@p:pointer
+ *Return:int
+ */
+int is_octal(va_list p)
+{
+int dig = 0;
+unsigned int x = va_arg(p, unsigned int);
+dig = base8(x, dig) + 1;
+return (dig);
+}
+/**
+ *base16_upper - converts to hexadecimal
+ *@x:int
+ *@dig: int
+ *Return: int
+ */
+int base16_upper(unsigned int x, int dig)
+{
+if (x / 16)
+dig = base16_upper(n / 16, dig + 1);
+if ((n % 16) < 10)
+_putchar(n % 16 + 48);
+else
+_putchar(n % 16 + 55);
+return (dig);
+}
+/**
+ *is_hexa_upper - prints the hexadecimal
+ *@p:pointer
+ *Return: int
+ */
+int is_hexa_upper(va_list p)
+{
+int dig = 0;
+unsigned int x = va_arg(p, unsigned int);
+dig = base16_upper(x, dig) + 1;
+return (length);
+}
+/**
+ *base16_down - convertes to hexa
+ *@x:int
+ *@dig:int
+ *Return:int
+ */
+int base16_down(unsigned int x, int dig)
+{
+if (x / 16)
+dig = base16_down(n / 16, dig + 1);
+if ((n % 16) < 10)
+_putchar(n % 16 + 48);
+else
+_putchar(n % 16 + 87);
+return (dig);
+}
+/**
+ *is_hexa_down - prints the hexadecimal
+ *@p:pointer
+ *Return:int
+ */
+int is_hexa_down(va_list p)
+{
+int dig = 0;
+unsigned int x = va_arg(p, unsigned int);
+dig = base16_down(x, dig) + 1;
+return (length);
+}
